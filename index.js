d19ac57c5427c72df797f4a1446d3d5fd824db49
Rick Harris <rick.c.harris@gmail.com>
2017-01-30 11:35:24 -0800

    Add exception for mixed operators

--- index.js
+++ index.js
@@ -3,5 +3,14 @@ module.exports = {
   extends: [ "react-app" ],
   env: { browser: false },
   plugins: [ "prettier" ],
-  rules: { "prettier/prettier": "error", "import/no-webpack-loader-syntax": 0 }
+  rules: {
+    // This doesn't work for us because we use `inject-loader` for testing JS
+    // files, and can't be automatically configured in webpack.config.js.
+    "import/no-webpack-loader-syntax": "off",
+    // Prettier automatically uses the least amount of parens possible, so this
+    // does more harm than good.
+    "no-mixed-operators": "off",
+    // Enforce that code is formatted with prettier.
+    "prettier/prettier": "error"
+  }
 };
