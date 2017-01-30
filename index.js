a13e6f795a59c68ebd13b5c5b874f55b92d26f6d
Rick Harris <rick.c.harris@gmail.com>
2017-01-29 21:58:46 -0800

    Switch from airbnb to create-react-app's eslint config + prettier

--- index.js
+++ index.js
@@ -1,28 +1,7 @@
 module.exports = {
-  parser: 'babel-eslint',
-  extends: [
-    'airbnb',
-    'plugin:flowtype/recommended',
-    // overwrite airbnb and enable all import errors/warnings
-    'plugin:import/errors',
-    'plugin:import/warnings',
-  ],
-  env: {
-    browser: false,
-  },
-  plugins: [
-    'flowtype',
-    'import',
-    'react',
-  ],
-  rules: {
-    'react/prefer-stateless-function': 0,
-    'react/jsx-filename-extension': 0,
-    'import/prefer-default-export': 0,
-    'global-require': 0,
-    'no-console': 0,
-    'func-names': 0,
-    'object-curly-spacing': [1, 'never'],
-    'import/no-webpack-loader-syntax': 0,
-  },
+  parser: "babel-eslint",
+  extends: [ "react-app" ],
+  env: { browser: false },
+  plugins: [ "prettier" ],
+  rules: { "prettier/prettier": "error", "import/no-webpack-loader-syntax": 0 }
 };
