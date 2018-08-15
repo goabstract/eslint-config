b77a9e54c65210d1590436f0f3bedd665ce0e7f9
Rick Harris <rickharris@users.noreply.github.com>
2018-08-14 17:07:37 -0700

    Add eslint-plugin-mocha to prevent exclusive mocha tests

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ module.exports = {
   parser: "babel-eslint",
   extends: ["react-app", "plugin:import/errors", "plugin:import/warnings"],
   env: { browser: false },
-  plugins: ["prettier"],
+  plugins: ["mocha", "prettier"],
   rules: {
     // This doesn't work for us because we use `inject-loader` for testing JS
     // files, and can't be automatically configured in webpack.config.js.
     "import/no-webpack-loader-syntax": "off",
     "import/order": "warn",
+    "mocha/no-exclusive-tests": "error",
     // Prettier automatically uses the least amount of parens possible, so this
     // does more harm than good.
     "no-mixed-operators": "off",
